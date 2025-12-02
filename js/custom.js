c2b4f2f1aa411196221cf0339919e40865e52d31
leehanle <leehanle12345@naver.com>
2025-12-02 16:28:53 +0900

    hanleportfolio1

---
+++ js/custom.js
@@ -0,0 +1,34 @@
+$(function () {
+  $(".self").on("mouseenter", function () {
+    $(".self .leftwhitebox").stop().animate({ left: "-50%" }, 1500);
+    $(".self .rightwhitebox").stop().animate({ right: "-50%" }, 1500);
+  });
+  $(".self").on("mouseleave", function () {
+    $(".self .leftwhitebox").stop().animate({ left: "0%" }, 1500);
+    $(".self .rightwhitebox").stop().animate({ right: "0%" }, 1500);
+  });
+  $(".self .skil ul li").on("mouseenter", function () {
+    $(this).css({ "background-color": "rgba(93, 114, 255, 1)" });
+  });
+  $(".self .skil ul li").on("mouseleave", function () {
+    $(this).css({ "background-color": "rgba(93, 114, 255, 0.5)" });
+  });
+  $(".upai li").on("mouseenter", function () {
+    $(".downai li").css({ transform: "scale(0.7)", opacity: "0.5" });
+    $(".upai li").css({ transform: "scale(0.7)", opacity: "0.5" });
+    $(this).css({ transform: "scale(1.3)", opacity: "1" });
+  });
+  $(".upai li").on("mouseleave", function () {
+    $(".downai li").css({ transform: "scale(1)", opacity: 1 });
+    $(".upai li").css({ transform: "scale(1)", opacity: "1" });
+  });
+  $(".downai li").on("mouseenter", function () {
+    $(".upai li").css({ transform: "scale(0.7)", opacity: "0.5" });
+    $(".downai li").css({ transform: "scale(0.7)", opacity: "0.5" });
+    $(this).css({ transform: "scale(1.3)", opacity: "1" });
+  });
+  $(".downai li").on("mouseleave", function () {
+    $(".upai li").css({ transform: "scale(1)", opacity: 1 });
+    $(".downai li").css({ transform: "scale(1)", opacity: "1" });
+  });
+});
